90c8b9eae739df65e0437cc9a8fc0427b9e8eaa5
huanghao618 <1098385916@qq.com>
2021-04-16 19:36:08 +0800

    a

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
           Edit <code>src/App.js</code> and save to reload. Learn React Learn
           React Learn React
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Learn React Learn React Learn React Learn React Learn
-          React Learn React Learn React
-        </a>
+          Learn React Learn React Learn React Learn React Learn React
+          Learn"dev"" React Learn React Learn React
+        </a> */}
       </header>
     </div>
   );
